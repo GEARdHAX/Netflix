d3737230e112366785876ba57179580fc3a3e20d
GEARdHAX <81669068+GEARdHAX@users.noreply.github.com>
2023-06-05 13:17:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('resize', function() {
+ if (window.innerWidth <= 768) {
+  document.getElementsByClassName("btn btn-secondary dropdown-toggle")[0].innerHTML = "🌏︎";
+ }
+ else{
+  document.getElementsByClassName("btn btn-secondary dropdown-toggle")[0].innerHTML = "🌏︎ English";
+  
+ }
+});
+const image = document.getElementsByClassName("logo")[0];
+
+image.addEventListener('click', function() {
+  location.reload();
+});
