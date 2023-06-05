798a86ccc2e63985a943ec530b95b2f263015602
GEARdHAX <81669068+GEARdHAX@users.noreply.github.com>
2023-06-05 15:57:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ const image = document.getElementsByClassName("logo")[0];
 image.addEventListener('click', function() {
   location.reload();
 });
+document
+ .getElementsByClassName("arrow")[0]
+ .addEventListener("click", function () {
+  if (
+   window.pageYOffset ===
+   document.documentElement.scrollHeight - window.innerHeight
+  ) {
+   // Reached bottom of the page
+   window.scrollTo(0, 0); // Scroll to the top
+  } else {
+   window.scrollBy(0, window.innerHeight); // Scroll one viewport height
+  }
+ });
